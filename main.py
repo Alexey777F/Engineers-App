8a81961330916f90f4f5235c172a2575b0a0c7d9
Alexey <112414906+Alexey777F@users.noreply.github.com>
2024-03-06 18:46:58 +0300

    Add decorator set_engineer_session to file main.py

--- main.py
+++ main.py
@@ -9,3 +9,23 @@ from sqlalchemy.exc import OperationalError
 from logger_conf import Logger
 
 logger = Logger()
+
+
+def set_engineer_session(func: Callable) -> Callable:
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        try:
+            if 'username' in session:
+                username = session['username']
+                engineer_info = Engineer.get_engineer_data(username)
+                if engineer_info:
+                    engineer_name = ' '.join(engineer_info[0:3])
+                    working_position = engineer_info[3]
+                    short_name = engineer_info[1][0] + '. ' + engineer_info[0]
+                    return func(username, engineer_name, working_position, short_name, *args, **kwargs)
+        except OperationalError as e:
+            logger.error(f"Ошибка соединения с бд! {e}")
+            return redirect(url_for('db_error'))
+        return redirect(url_for('login'))
+    return wrapper
+
